34581d976cc5a2261f7595813f4c728387cdeff8
Iaroslav Rykhalskyi <rikhalskiy@gmail.com>
2025-05-11 22:47:58 +0200

    #6 Calculate resources

--- backend/CastleCommander.WebApi/CastleCommander.WebApi/GameLogic/Turns/ResourceCollectionTurn.cs
+++ backend/CastleCommander.WebApi/CastleCommander.WebApi/GameLogic/Turns/ResourceCollectionTurn.cs
@@ -8,7 +8,34 @@
         {
             base.MakeTurn(userInput, game);
             game.Dice = DiceHelper.Throw();
+            CollectResources(game);
         }
 
+        private void CollectResources(Game game)
+        {
+            for (int i = 1; i < game.Castle.Hexagons.Count; i++) {
+                foreach (var facility in game.Castle.Hexagons[i].Facilities)
+                {
+
+                    game.Players[facility.PlayerId].Resources[i-1].Number += GetResourceFromFacility(facility);
+                }
+            }
+        }
+
+        private int GetResourceFromFacility(Facility facility)
+        {
+            switch (facility.Size)
+            {
+                case FacilitySize.Small:
+                    return 1;
+                case FacilitySize.Medium:
+                    return 2;
+                case FacilitySize.Large:
+                    return 4;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+        }
     }
 }
